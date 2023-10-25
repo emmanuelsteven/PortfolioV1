225e57165a096d48153776dc683b85316a3286dd
emmanuelsteven <write2stevemmy@gmail.com>
2023-10-25 17:59:22 +0100

    project cards

---
+++ portfolio_v1/src/components/projects/Data.jsx
@@ -0,0 +1,35 @@
+import heliHire  from '../../assets/images/heliHire.png';
+import ema from  '../../assets/images/ema.jpg';
+
+export const Data = [
+    {
+        title:'Heli Hire',
+        description:'HeliHire is a website where users can book a Helicopters, cancel a booking. The user can also see the details of a Helicopter, show all your reserved helicopter tours. however a user require authentication to make reservation users is expected to register an account. Then you can reserve any number of available tours and cancel the reservation anytime.',
+        projectLink:'https://heli-hire-captsone-frontend.vercel.app/',
+   codeLink:'https://github.com/emmanuelsteven/heli-hire-captone-backend',
+        image:  <Image scr ={ema} alt = 'heliHire' />,
+        tags:['ReactJS','All'],
+        stacks:['ReactJS','antD','Ruby','Ruby on Rails','postgreSQL'],
+
+    },
+    {
+        title:'Heli Hire',
+        description:'HeliHire is a website where users can book a Helicopters, cancel a booking. The user can also see the details of a Helicopter, show all your reserved helicopter tours. however a user require authentication to make reservation users is expected to register an account. Then you can reserve any number of available tours and cancel the reservation anytime.',
+        projectLink:'https://heli-hire-captsone-frontend.vercel.app/',
+        sourceLink:'https://github.com/emmanuelsteven/heli-hire-captone-backend',
+        image:  <Image scr ={heliHire} alt = 'heliHire' />,
+        tags:['ReactJS','All'],
+        stacks:['ReactJS','antD','Ruby','Ruby on Rails','postgreSQL'],
+
+    },
+    {
+        title:'Heli Hire',
+        description:'HeliHire is a website where users can book a Helicopters, cancel a booking. The user can also see the details of a Helicopter, show all your reserved helicopter tours. however a user require authentication to make reservation users is expected to register an account. Then you can reserve any number of available tours and cancel the reservation anytime.',
+        projectLink:'https://heli-hire-captsone-frontend.vercel.app/',
+        sourceLink:'https://github.com/emmanuelsteven/heli-hire-captone-backend',
+        image:  <Image scr ={heliHire} alt = 'heliHire' />,
+        tags:['ReactJS','All'],
+        stacks:['ReactJS','antD','Ruby','Ruby on Rails','postgreSQL'],
+
+    }
+]
