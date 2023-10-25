d86836254384295b95eb9abbcb57a647cc55807f
emmanuelsteven <write2stevemmy@gmail.com>
2023-10-26 00:49:07 +0100

    fix node_module

---
+++ src/components/Home.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import remImage from '../assets/images/Em.png';
+// import { TypeAnimation } from 'react-type-animation'
+import TypedAnimation from './TypedAnimation';
+import { FaTwitter, FaFacebookF,FaLinkedinIn, FaGithub} from 'react-icons/fa'
+
+const Home = () => {
+  return (
+    <div className='flex items-center justify-between w-full'>
+      <div className='flex items-center justify-between w-full'>
+         <img className='w-50 h-screen object-cover objec-left scale-x-[-1]'src={remImage} alt="invalid" />
+         </div>
+         <div className='w-30 m-auto h-full  flex flex-col justify-center lg:items-start items-center'>
+      <h1 className='sm:text-5xl text-4xl font-bold text-gray-800  mb-4'>Turning ideas into substance with codes and designs </h1> 
+      <p className='flex flex-col sm:text-2xl text-1xl pt-4 text-gray-800  m-1'>As a skilled full-stack developer , i am dedicated to turning ideas into innovative web applications.Explore my latest projects and publications, showcasing my expertise...would love to hear from you 
+   </p>
+   <TypedAnimation /> 
+   <div className=' flex justify-between pt-6 max-w-[200px] w-full'>
+   <FaTwitter className='cursor-pointer' size={20} />
+   <FaFacebookF className='cursor-pointer' size={20} />
+   <FaLinkedinIn  className='cursor-pointer' size={20}/>
+   <FaGithub className='cursor-pointer' size={20} />
+   </div>
+      </div>
+    </div> 
+  )
+}
+
+export default Home
